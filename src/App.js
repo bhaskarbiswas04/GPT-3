824dbef6689662a0530251a62808d6f63c1c19d2
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2023-01-25 12:46:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+
+import {Header, WhatGPT, Features, Possibilities, Blog, Footer} from './containers'
+import {Navbar, CTA, Brands} from './components';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <div className='gradient__bg'>
+            <Navbar />
+            <Header />
+        </div>
+        <Brands />
+        <WhatGPT />
+        <Features />
+        <Possibilities />
+        <CTA />
+        <Blog />
+        <Footer />
     </div>
-  );
+    
+  )
 }
 
-export default App;
+export default App
